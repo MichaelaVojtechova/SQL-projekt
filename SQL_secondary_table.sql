995281c9f85814be89953eeec7844017dfcc8931
Michaela Vojtechova <bartlovam@gmail.com>
2023-04-18 17:09:58 +0200

    Úprava na evropské státy.

--- SQL_secondary_table.sql
+++ SQL_secondary_table.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE t_Michaela_Vojtechova_project_SQL_secondary_final AS
+CREATE OR REPLACE TABLE t_Michaela_Vojtechova_project_SQL_secondary_final AS
 SELECT 
 	c.country,
 	c.continent,
@@ -12,5 +12,9 @@ FROM countries c
 JOIN economies e 
 	ON c.country = e.country
 WHERE e.`year` >= 2006
-	AND e.`year` <= 2018;
+	AND e.`year` <= 2018
+	AND c.continent = 'Europe';
+
+SELECT *
+FROM t_michaela_vojtechova_project_sql_secondary_final sec;
 	
