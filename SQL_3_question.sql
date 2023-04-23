e1debcb35a8ed38ff4192fc48fdbf9d02477e9dd
Michaela Vojtechova <bartlovam@gmail.com>
2023-04-23 23:12:02 +0200

    Úprava řádků

--- SQL_3_question.sql
+++ SQL_3_question.sql
@@ -23,17 +23,6 @@ LEFT JOIN (
 	FROM t_michaela_vojtechova_project_sql_primary_final prim
 	GROUP BY prim.food_name, prim.price_year) price2
 	ON price.food_name = price2.food_name
-	AND price.price_year = price2.price_year + 1;
-
--- ekvivalentní --
-
-SELECT
-	prim.food_name,
-	prim.price_year,
-	round(avg(prim.food_price), 2) AS avg_price,
-	LAG(round(avg(prim.food_price), 2), 1) OVER (PARTITION BY prim.food_name ORDER BY prim.price_year) AS price_previous_year,
-	round((round(avg(prim.food_price), 2) - LAG(round(avg(prim.food_price), 2), 1) OVER (PARTITION BY prim.food_name ORDER BY prim.price_year) ) / LAG(round(avg(prim.food_price), 2), 1) OVER (PARTITION BY prim.food_name ORDER BY prim.price_year) * 100, 2) AS price_growth
-FROM t_michaela_vojtechova_project_sql_primary_final prim
-GROUP BY prim.food_name, prim.price_year
--- ORDER BY price_growth ASC
+	AND price.price_year = price2.price_year + 1
+WHERE price.price_year != '2006'
 ;
