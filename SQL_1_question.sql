fa4d05bcf4f13ce1cd21e42844978bec88e2c9cd
Michaela Vojtechova <bartlovam@gmail.com>
2023-04-23 22:54:04 +0200

    Přidán nový sloupec.

--- SQL_1_question.sql
+++ SQL_1_question.sql
@@ -4,9 +4,25 @@
  */
 
 SELECT 
-	prim.branch_name,
-	prim.payroll_year,
-	round(avg(prim.payroll_value)) AS avg_payroll
-FROM t_Michaela_Vojtechova_project_SQL_primary_final prim
-GROUP BY prim.branch_name, prim.payroll_year;
+	pay.branch_name,
+	pay.payroll_year,
+	pay.average_payroll,
+	round((pay.average_payroll - pay2.average_payroll) / pay2.average_payroll * 100, 2) AS percent_year_growth
+FROM (
+	SELECT
+		prim.branch_name,
+		prim.payroll_year,
+		round(avg(prim.payroll_value)) AS average_payroll
+	FROM t_Michaela_Vojtechova_project_SQL_primary_final prim
+	GROUP BY prim.branch_name, prim.payroll_year) pay
+LEFT JOIN (
+	SELECT
+		prim.id_payroll,
+		prim.branch_name,
+		prim.payroll_year,
+		round(avg(prim.payroll_value)) AS average_payroll
+	FROM t_Michaela_Vojtechova_project_SQL_primary_final prim
+	GROUP BY prim.branch_name, prim.payroll_year) pay2
+	ON pay.branch_name = pay2.branch_name
+	AND pay.payroll_year = pay2.payroll_year + 1;
 
